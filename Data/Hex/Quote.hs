57b3c53f23466107b651db559940b7d55fb6bb20
Keegan McAllister <mcallister.keegan@gmail.com>
2011-09-14 06:34:07 -0400

    document

--- Data/Hex/Quote.hs
+++ Data/Hex/Quote.hs
@@ -3,7 +3,10 @@
   , NamedFieldPuns
   , CPP #-}
 module Data.Hex.Quote
-    ( hex
+    ( -- * The quasiquoter
+      hex
+      -- * Helper functions
+    , parseHex
     ) where
 
 import Control.Arrow
@@ -35,6 +38,7 @@ hexMap = IM.fromList . map (first ord) $ concat
     , zip ['A'..'F'] [10..15]
     , zip ['a'..'f'] [10..15] ]
 
+-- | The hexadecimal parser used for @'hex'@ expressions.
 parseHex :: String -> [Word8]
 parseHex = pair . catMaybes . map get . dropComments where
     get v = IM.lookup (ord v) hexMap
@@ -92,6 +96,56 @@ hexPat :: String -> Q Pat
 hexPat xs = case parse parseToks "Data.Hex.Quote pattern" (dropComments xs) of
     Left  e -> error (show e)
     Right v -> mkPat v
+
+{- |
+
+As an expression, the @'hex'@ quasiquoter provides hexadecimal @'B.ByteString'@
+literals:
+
+>import Data.Hex.Quote
+>import qualified Data.ByteString as B
+>
+>main = B.putStr [hex|
+>    57 65 2c 20 61 6c 6f 6e 65 20 6f 6e 20 65 61 72
+>    74 68 2c 20 63 61 6e 20 72 65 62 65 6c 20 61 67
+>    61 69 6e 73 74 20 74 68 65 20 74 79 72 61 6e 6e
+>    79 20 6f 66 20 74 68 65 20 73 65 6c 66 69 73 68
+>    20 72 65 70 6c 69 63 61 74 6f 72 73 2e 0a |]
+
+All characters other than @0123456789abcdefABCDEF@ are ignored, including
+whitespace.  Comments start with \"@--@\" and continue to end-of-line:
+
+>code = [hex|
+>    7e3a          -- jle  0x3c
+>    4889f5        -- mov  rbp, rsi
+>    bb01000000    -- mov  ebx, 0x1
+>    488b7d08 |]   -- mov  rdi, [rbp+0x8]
+
+When using @'hex'@ as a pattern, you can include placeholders of the form
+@\<name:size\>@, where
+
+* @name@ is a Haskell identifier, or the wildcard pattern \"@_@\"
+
+* @size@ is the size of the field in bytes, or the word @rest@ to consume
+  the rest of the @'B.ByteString'@.
+
+The named placeholders bind local variables of type @'B.ByteString'@.  Here's
+an example of pattern-matching an IPv4-over-Ethernet-II frame:
+
+>import Data.Hex.Quote
+>
+>describe [hex|
+>    <src_mac:6> <dst_mac:6> 08 00  -- ethernet header
+>    45 <_:1> <len:2>               -- start of IP header
+>    <_:rest>                       -- discard remaining frame
+>  |] = (src_mac, dst_mac, len)
+>
+>describe _ = error "unknown frame"
+
+Quasiquotes require the @QuasiQuotes@ extension.  In pattern context, @'hex'@ also
+requires the @ViewPatterns@ extension.
+
+-}
 
 hex :: QuasiQuoter
 hex = QuasiQuoter
